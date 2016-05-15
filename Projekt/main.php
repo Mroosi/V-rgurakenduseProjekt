dd42fbedf1663aa7a8fbab1a5b8d1dc17369fbba
Mroosi <madisroosi@hotmail.com>
2016-05-15 22:24:34 +0300

    Algus tehtud

---
+++ Projekt/main.php
@@ -0,0 +1,31 @@
+﻿<?php
+    if (!isset($_SESSION)) session_start();
+    $_SESSION["kasutaja"] = 0;
+    require_once('header.html');
+        if( !empty($_GET)){
+             $vaartus = $_GET;
+        }else{
+             $vaartus = array('pealeht');
+             echo "mingijama";
+             echo implode($_GET);
+        }
+      
+        $leht = implode($vaartus);
+        if(empty($_POST)){
+        switch ($leht) {
+        case 'pealeht':
+        require('pealeht.html');
+        break;
+        
+        case 'logi':
+        require('logi.php');
+        break;
+        
+        case 'minu':
+           require('minu.php');
+        break;
+        
+    }
+  }
+    require_once('footer.html');
+?>
---
+++ Projekt/minu.php
@@ -0,0 +1,7 @@
+﻿<?php
+
+
+
+
+require_once('minu.html');
+?>
