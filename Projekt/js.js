c6d444031c3cbef26dae97d0c7894417df5216ab
Mroosi <madisroosi@hotmail.com>
2016-05-29 01:06:29 +0300

    kujundus...

---
+++ Projekt/js.js
@@ -0,0 +1,17 @@
+﻿window.onload = function() {
+var kuva = document.getElementById('log');
+var list = document.getElementById('logboxx');
+var rkuva = document.getElementById('reg');
+var rlist = document.getElementById('regbox');
+
+kuva.addEventListener('click', function(event) {
+    list.style.display = 'block';
+    rlist.style.display = 'none';
+});
+
+rkuva.addEventListener('click', function(event) {
+    rlist.style.display = 'block';
+    list.style.display = 'none';
+});
+
+}
